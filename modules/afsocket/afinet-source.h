e4285352bf676bb3de462082b5e4342962c9d41b
Kokan <kokaipeter@gmail.com>
2020-02-26 11:15:49 +0100

    afsocket: cleanup of dead code connected tcp/udp/tls
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- modules/afsocket/afinet-source.h
+++ modules/afsocket/afinet-source.h
@@ -38,10 +38,6 @@ typedef struct _AFInetSourceDriver
 
 void afinet_sd_set_tls_context(LogDriver *s, TLSContext *tls_context);
 
-AFInetSourceDriver *afinet_sd_new_tcp(GlobalConfig *cfg);
-AFInetSourceDriver *afinet_sd_new_tcp6(GlobalConfig *cfg);
-AFInetSourceDriver *afinet_sd_new_udp(GlobalConfig *cfg);
-AFInetSourceDriver *afinet_sd_new_udp6(GlobalConfig *cfg);
 AFInetSourceDriver *afinet_sd_new_syslog(GlobalConfig *cfg);
 AFInetSourceDriver *afinet_sd_new_network(GlobalConfig *cfg);
 
